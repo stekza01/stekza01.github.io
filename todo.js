25293edaf32eb776fdb5c2640d84880d51ca609e
stekza01 <stekza01@luther.edu>
2015-10-19 12:57:39 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,13 @@
+var myol;
+myol = document.querySelector("#myol");
 
+addtodo = function(){
+  var myp, mysize, mystr, btn, mine;
+  myp = document.querySelector("#myol");
+  mytext = document.querySelector("#mytext");
+  mypriority = document.querySelector("#priority");
+  btn = document.createElement("LI");
+  mine = document.createTextNode(mytext);
+  btn.appendChild(mine);
+  myp.appendChild(btn);
+}
