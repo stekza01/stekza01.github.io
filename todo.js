7ecf7258c41b7078b8875d2c792d17a9010bd80b
stekza01 <stekza01@luther.edu>
2015-10-21 12:41:29 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -3,11 +3,30 @@ myol = document.querySelector("#myol");
 
 addtodo = function(){
   var myp, mysize, mystr, btn, mine;
-  myp = document.querySelector("#myol");
+  myp = document.querySelector("#myul");
   mytext = document.querySelector("#mytext");
-  mypriority = document.querySelector("#priority");
-  btn = document.createElement("LI");
+  mypriority = document.querySelector("#priority").value;
   mine = document.createTextNode(mytext);
+  checkbox = document.createElement("INPUT");
+  btn = checkbox = document.createElement("li")
+  
+  myp.setAttribute("style", "list-style: none;");
+  
+  
+  
+  checbox.setAttribute("type", "checkbox");
+  mine.setAttribute("class", mypriority);
+  
   btn.appendChild(mine);
+  
   myp.appendChild(btn);
+}
+
+
+doneTask = function() {
+  if (this.checked){
+    this.parentNode.classList.add("done")}
+  else{
+    this.parentNode.classList.remove("done")
+  }
 }
