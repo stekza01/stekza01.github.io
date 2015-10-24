18233183efa6a2c1c2343d6e6981567b81494680
stekza01 <stekza01@luther.edu>
2015-10-23 19:04:06 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@
 addtodo = function(){
   var myp, mysize, mystr, myli, mine, checkbox, mypriority;
   myp = document.querySelector("#myul");
-  mytext = document.querySelector("#mytext");
+  mytext = document.querySelector("#mytext").value;
   mypriority = (document.querySelector("#priority").value);
   mine = document.createTextNode(mytext);
   checkbox = document.createElement("INPUT");
@@ -29,9 +29,11 @@ addtodo = function(){
 
 doneTask = function() {
   if (this.checked){
-    this.parentNode.classList.add("done")}
+    this.parentNode.classList.add("done");
+    localSave("myul");}
   else{
-    this.parentNode.classList.remove("done")
+    this.parentNode.classList.remove("done");
+    localSave("myul");
   }
 }
 
